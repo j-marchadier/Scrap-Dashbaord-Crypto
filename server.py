e704f4385a8101ec14699e9384bd1d8d56856d43
jmarchadier <julien.marchadier@edu.esiee.fr>
2022-01-27 17:25:24 +0100

    ok

--- server.py
+++ server.py
@@ -11,53 +11,48 @@ from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__, template_folder='./templates')
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def home():
+    return redirect(url_for('graphCrypto', crypto="bitcoin"))
+
+
+@app.route("/graph/<crypto>", methods=['GET', 'POST'])
+def graphCrypto(crypto):
     if request.method == 'GET':
-        # On recupere les donners dans notre mongoDB
-        df = pd.DataFrame(list(db["ethereum"].find()))[["date", "marketcap", "volume", "open"]]
-
+        df = pd.DataFrame(list(db[str(crypto)].find()))[["date", "marketcap", "volume", "open"]]
         # Graphine line of Ethereum value
-        fig = px.line(df, x='date', y='open', markers=True)
-        fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)', 'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
-        etherValue = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+        graph = []
+        for i in ("open", "marketcap", "volume"):
+            fig = px.line(df, x='date', y=i, title=i + " value of " + crypto)
+            fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)', 'paper_bgcolor': 'rgba(0, 0, 0, 0)'},
+                              title_font_color='#E6B11B', legend_font_color="White")
+            fig.update_traces(line_color='#E6B11B')
+            fig.update_yaxes(showgrid=False, color="White")
+            fig.update_xaxes(showgrid=False, tickangle=45, color="White")
+            graph.append(json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder))
 
-        # Graphine line of Ethereum MarketCap
-        fig = px.line(df, x='date', y='marketcap', markers=True)
-        fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)', 'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
-        etherMK = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-
-        # Graphine line of Ethereum volume
-        fig = px.line(df, x='date', y='marketcap', markers=True)
-        fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)', 'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
-        etherVolume = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-
-        return render_template('template_home_test.html', graphJSON=etherValue, graphJSON2=etherMK,
-                               graphJSON3=etherVolume, data=db.list_collection_names())
+        return render_template('template_home_test.html', graphJSON=graph[0], graphJSON2=graph[1],
+                               graphJSON3=graph[2], data=db.list_collection_names())
 
     elif request.method == 'POST':
         data = request.form.get("comp_select")
         return redirect(url_for('graphCrypto', crypto=data))
 
 
-@app.route("/predict")
-def predict():
-    graphJSON = IA()
-    return render_template('template_predict_test.html', graphJSON=graphJSON, data=db.list_collection_names())
+@app.route("/graph/<crypto>/predict", methods=['GET', 'POST'])
+def predict(crypto):
+    if request.method == 'GET':
+        graphJSON = IA(str(crypto))
+
+        return render_template('template_predict_test.html', graphJSON=graphJSON, data=db.list_collection_names())
+
+    elif request.method == 'POST':
+        data = request.form.get("comp_select")
+        return redirect(url_for('predict', crypto=data))
 
 
-@app.route("/graph/<crypto>", methods=['GET', 'POST'])
-def graphCrypto(crypto):
-    df = pd.DataFrame(list(db[str(crypto)].find()))[["date", "marketcap", "volume", "open"]]
-    fig = px.line(df, x='date', y='open', markers=True)
-    fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)', 'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
-    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-
-    return render_template('template_home_test.html', graphJSON=graphJSON, data=db.list_collection_names())
-
-
-def IA():
-    df = pd.DataFrame(list(db["ethereum"].find()))[["date", "marketcap", "volume", "open"]]
+def IA(crypto):
+    df = pd.DataFrame(list(db[crypto].find()))[["date", "marketcap", "volume", "open"]]
     df = df.iloc[::-1].reset_index()
     df["date"] = pd.to_datetime(df["date"], format='%Y-%m-%d')
 
@@ -97,6 +92,11 @@ def IA():
     df["arima"] = y_pred_out_arima
 
     fig = px.line(df, x='date', y=["train", "test", "sarimax", "arima"])
+    fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)', 'paper_bgcolor': 'rgba(0, 0, 0, 0)'},
+                      title_font_color='#E6B11B', legend_font_color="White")
+    #fig.update_traces(line_color='#E6B11B')
+    fig.update_yaxes(showgrid=False, color="White")
+    fig.update_xaxes(showgrid=False, tickangle=45, color="White")
     return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
 
